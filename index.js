1634165ae8d112fab1a80cd1d36d056f673e9d2f
mzh <manzhenhua@sunlands.com>
2018-11-26 16:36:12 +0800

    修改git地址

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ program.version('1.0.0', '-v, --version')
             ]).then((answers) => {
                 const spinner = ora('正在下载模板...');
                 spinner.start();
-                download('direct:https://github.com/xkl2013/man-admin.git', name, {clone: true}, (err) => {
+                download('direct:https://github.com/xkl2013/admin-cli.git', name, {clone: true}, (err) => {
                     if(err){
                         spinner.fail();
                         console.log(symbols.error, chalk.red(err));
